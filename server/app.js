918703e901ae836a13f032e58270701c32a2f0de
flowtyf <taiao.kawiti.wbhs@gmail.com>
2023-08-12 22:34:20 +1200

    Update server to use pool connection, update post request to use req.body for values instead of req

--- server/app.js
+++ server/app.js
@@ -2,7 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const got = require('got');
-import * as db from './db/index.js'
+
+const { Pool } = require('pg');
+const pool = new Pool({
+  user: 'username',
+  host: 'localhost',
+  database: 'd',
+  password: 'pass',
+  port: 5432,
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
@@ -11,58 +19,54 @@ app.listen(port, () => {
 //user, community, event
 
 app.get('/user/:id', async (req, res) => {
-  const result = await db.query('SELECT * FROM user WHERE id = $1', [
+  const result = await pool.query('SELECT * FROM user WHERE id = $1', [
     req.params.id,
   ]);
   res.send(result.rows[0]);
 });
 
 app.post('/user/:id', async (req, res) => {
-  const result = await db.query('INSERT INTO user VALUES', [
-    req.params.id,
-    req.name,
-    req.age,
-    req.email,
-    req.university,
-    req.interests,
-    req.languages,
-  ]);
+  const { name, age, email, university, interests, languages } = req.body;
+  const result = await pool.query(
+    'INSERT INTO user VALUES $1, $2, $3, $4, $5, $6, $7',
+    [req.params.id, name, age, email, university, interests, languages]
+  );
   res.send(result.rows[0]);
 });
 
 app.get('/events', async (req, res) => {
-  const result = await db.query('SELECT * FROM event');
+  const result = await pool.query('SELECT * FROM event');
   res.send(result.rows[0]);
 });
 
 app.get('/events/:id', async (req, res) => {
-  const result = await db.query('SELECT * FROM event WHERE id = $1', [
+  const result = await pool.query('SELECT * FROM event WHERE id = $1', [
     req.params.id,
   ]);
   res.send(result.rows[0]);
 });
 
 app.get('/communities', async (req, res) => {
-  const result = await db.query('SELECT * FROM community');
+  const result = await pool.query('SELECT * FROM community');
   res.send(result.rows[0]);
 });
 
 app.get('/communities/:id', async (req, res) => {
-  const result = await db.query('SELECT * FROM community WHERE id = $1', [
+  const result = await pool.query('SELECT * FROM community WHERE id = $1', [
     req.params.id,
   ]);
   res.send(result.rows[0]);
 });
 
 app.get('/eventfinda-api', async (req, res) => {
-//   let user = "summeroftechcreatecamp"
-//   let pass = "tcr5sxb84vxj"
-//   const {data} = await got.get('https://api.eventfinda.co.nz/v2/events.xml', {
-//     headers: {
-//       "Authorization":
-//     }
-// }).json();
-// console.log(data)
+  //   let user = "summeroftechcreatecamp"
+  //   let pass = "tcr5sxb84vxj"
+  //   const {data} = await got.get('https://api.eventfinda.co.nz/v2/events.xml', {
+  //     headers: {
+  //       "Authorization":
+  //     }
+  // }).json();
+  // console.log(data)
 
-var spawn = require("child_process").spawn;
+  var spawn = require('child_process').spawn;
 });
--- server/db/index.js
+++
@@ -1,7 +0,0 @@
-import { Pool } from 'pg'
- 
-const pool = new Pool()
- 
-export const query = (text, params, callback) => {
-  return pool.query(text, params, callback)
-}
