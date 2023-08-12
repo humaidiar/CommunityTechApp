205671e1ce74009a5c5646242c90ccf98b6ca8d0
NZpatelK <patelk.dev@hotmail.com>
2023-08-13 11:25:02 +1200

    add card feature

--- event-app-prototype/src/Components/Home.js
+++ event-app-prototype/src/Components/Home.js
@@ -3,12 +3,13 @@ import Navbar from './NavBar';
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/effect-coverflow";
-import "swiper/css/pagination";
+// import "swiper/css/pagination";
 // import "swiper/css/navigation";
 import "swiper/css/autoplay";
 import "swiper/css/scrollbar";
 import '../Style/HomePage.css'
-import { Pagination } from 'swiper/modules';
+// import { Pagination } from 'swiper/modules';
+import peopleIcon from '../Images/people.png'
 
 const Home = () => {
     const [isLogin, setIsLogin] = React.useState(true);
@@ -26,17 +27,17 @@ const Home = () => {
 
 
                         <Swiper
-                         slidesPerView={3}
-                         spaceBetween={30}
-                         pagination={{
-                           clickable: true,
-                         }}
-                         modules={[Pagination]}
-                         className="mySwiper"
+                            slidesPerView={4}
+                            spaceBetween={30}
+                            pagination={{
+                                clickable: true,
+                            }}
+                            //  modules={[Pagination]}
+                            className="mySwiper"
                         >
                             {/* List of Tetimoial */}
 
-                            { Array.from({ length: 10 }).map((_, index) => {
+                            {Array.from({ length: 10 }).map((_, index) => {
                                 return (
 
                                     <SwiperSlide className="swiperSlide" key={index}>
@@ -48,10 +49,18 @@ const Home = () => {
                                             </div>
                                             <div className="interestContent">
                                                 <h3>Photograpghy</h3>
-                                                <p>Photography is the art, application, and practice of creating durable images by recording light, either electronically by means of an image sensor, or chemically by means of a light-sensitive material such as photographic film.</p>
-
+                                                {/* <p>simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p> */}
+                                                <div className='attendAndincomingEvent'>
+                                                    <div className='attendEvent'>
+                                                        <img src={peopleIcon} alt=''/>
+                                                        <p>10 Members</p>
+                                                    </div>
+                                                    <div className='incomingEvent'>
+                                                        <p>18 Incoming Event</p>
+                                                    </div>
+                                                </div>
                                             </div>
-
+                                            <button className="joinBtn">Join</button>
 
                                         </div>
                                     </SwiperSlide>
@@ -63,6 +72,115 @@ const Home = () => {
                         </Swiper>
 
                     </div>
+
+                    <div className='homePageHeader sub'>
+                        <h1>Gaming</h1>
+                    </div>
+                    <div className='interestContainer'>
+
+
+                        <Swiper
+                            slidesPerView={4}
+                            spaceBetween={30}
+                            pagination={{
+                                clickable: true,
+                            }}
+                            //  modules={[Pagination]}
+                            className="mySwiper"
+                        >
+                            {/* List of Tetimoial */}
+
+                            {Array.from({ length: 10 }).map((_, index) => {
+                                return (
+
+                                    <SwiperSlide className="swiperSlide" key={index}>
+                                        <div className="interestBox">
+                                            <div className="interestImg">
+                                                <div className="interestIcon">
+                                                    Gaming
+                                                </div>
+                                            </div>
+                                            <div className="interestContent">
+                                                <h3>Gaming</h3>
+                                                {/* <p>simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p> */}
+                                                <div className='attendAndincomingEvent'>
+                                                    <div className='attendEvent'>
+                                                        <img src={peopleIcon} alt=''/>
+                                                        <p>10 Members</p>
+                                                    </div>
+                                                    <div className='incomingEvent'>
+                                                        <p>18 Incoming Event</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <button className="joinBtn">Join</button>
+
+                                        </div>
+                                    </SwiperSlide>
+
+
+                                );
+                            })}
+
+                        </Swiper>
+
+                    </div>
+
+
+                    <div className='homePageHeader sub'>
+                        <h1>Outdoor</h1>
+                    </div>
+                    <div className='interestContainer'>
+
+
+                        <Swiper
+                            slidesPerView={4}
+                            spaceBetween={30}
+                            pagination={{
+                                clickable: true,
+                            }}
+                            //  modules={[Pagination]}
+                            className="mySwiper"
+                        >
+                            {/* List of Tetimoial */}
+
+                            {Array.from({ length: 10 }).map((_, index) => {
+                                return (
+
+                                    <SwiperSlide className="swiperSlide" key={index}>
+                                        <div className="interestBox">
+                                            <div className="interestImg">
+                                                <div className="interestIcon">
+                                                Outdoor
+                                                </div>
+                                            </div>
+                                            <div className="interestContent">
+                                                <h3>Outdoor</h3>
+                                                {/* <p>simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p> */}
+                                                <div className='attendAndincomingEvent'>
+                                                    <div className='attendEvent'>
+                                                        <img src={peopleIcon} alt=''/>
+                                                        <p>10 Members</p>
+                                                    </div>
+                                                    <div className='incomingEvent'>
+                                                        <p>18 Incoming Event</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <button className="joinBtn">Join</button>
+
+                                        </div>
+                                    </SwiperSlide>
+
+
+                                );
+                            })}
+
+                        </Swiper>
+
+                    </div>
+
+
 
 
                 </div>
