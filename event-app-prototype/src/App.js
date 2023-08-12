f3bdb5ef52bfa2cf97f7b5fe61b95e2d084765d2
flowtyf <taiao.kawiti.wbhs@gmail.com>
2023-08-13 11:47:22 +1200

    Resolving conflict

--- event-app-prototype/src/App.js
+++ event-app-prototype/src/App.js
@@ -3,13 +3,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './Components/Home';
 import SignUp from './Components/SignUp';
 import QuestionInterest from './Components/QuestionInterest';
-import { QuestionBasedAuckland } from './Components/QuestionBasedAuckland';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<QuestionBasedAuckland />}/>
+        <Route path="/" element={<Home />}/>
         <Route path="signup" element={<SignUp />} />
         <Route path="questionInterest" element={<QuestionInterest />} />
       </Routes>
