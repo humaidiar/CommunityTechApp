ab5f5e6530ae376f6a1d9ab13432e1b9d6627cee
flowtyf <taiao.kawiti.wbhs@gmail.com>
2023-08-12 15:34:18 +1200

    Draft endpoints - most get requests and POST user

--- server/app.js
+++ server/app.js
@@ -1,19 +1,61 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const request = require('request')
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-app.get('/users/:id', (req, res) => {});
+//user, community, event
 
-app.post('/users/:id', (req, res) => {});
+app.get('/user/:id', async (req, res) => {
+  const result = await db.query('SELECT * FROM user WHERE id = $1', [
+    req.params.id,
+  ]);
+  res.send(result.rows[0]);
+});
 
-app.get('/events', (req, res) => {});
+app.post('/user/:id', async (req, res) => {
+  const result = await db.query('INSERT INTO user VALUES', [
+    req.params.id,
+    req.name,
+    req.age,
+    req.email,
+    req.university,
+    req.interests,
+    req.languages,
+  ]);
+  res.send(result.rows[0]);
+});
 
-app.get('/events/:id', (req, res) => {});
+app.get('/events', async (req, res) => {
+  const result = await db.query('SELECT * FROM event');
+  res.send(result.rows[0]);
+});
 
-app.get('/communities/:id', (req, res) => {});
+app.get('/events/:id', async (req, res) => {
+  const result = await db.query('SELECT * FROM event WHERE id = $1', [
+    req.params.id,
+  ]);
+  res.send(result.rows[0]);
+});
 
-app.get('/communities/:id', (req, res) => {});
+app.get('/communities', async (req, res) => {
+  const result = await db.query('SELECT * FROM community');
+  res.send(result.rows[0]);
+});
+
+app.get('/communities/:id', async (req, res) => {
+  const result = await db.query('SELECT * FROM community WHERE id = $1', [
+    req.params.id,
+  ]);
+  res.send(result.rows[0]);
+});
+
+app.get('/eventfinda-api', async (req, res) => {
+
+  
+
+})
